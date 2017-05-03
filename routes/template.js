04621b8fe5be921406d9ded8fda107ec32866124
Cooper Campbell <cooper.lax98@gmail.com>
2017-05-02 23:23:38 -0400

    Added router template

---
+++ routes/template.js
@@ -0,0 +1,16 @@
+/**
+*
+* THIS IS A TEMPLATE FILE. DO NOT CHANGE THIS. YOU ARE WELCOME TO COPY AND PASTE FROM THIS AT ANYTIME.
+*
+**/
+
+const controller = require('./../../controller/');
+const router     = require('express').Router();
+
+router.route('/')
+  .get((req, res) => {
+  })
+  .post((req, res) => {
+  })
+
+module.exports = router;
