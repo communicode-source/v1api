2b6f81757f5e649b783c374a7a474a3771562e76
Cooper Campbell <cpcampbell@immicorp.local>
2017-08-07 11:30:59 -0400

    Fixed weird glitch with prototypes

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -32,21 +32,21 @@ module.exports = (app) => {
       return false;
   }
 
-  Object.prototype.removeKeys = function(objectToRemove) {
-  		for(const i in this) {
-      		if(objectToRemove.indexOf(i) !== -1) {
-          		delete this[i];
-          }
-      }
-  }
-
-  Object.prototype.onlyKeys = function(objectToKeep) {
-  		for(const i in this) {
-      		if(objectToKeep.indexOf(i) === -1) {
-          		delete this[i];
-          }
-      }
-  }
+  // Object.prototype.removeKeys = function(objectToRemove) {
+  // 		for(const i in this) {
+  //     		if(objectToRemove.indexOf(i) !== -1) {
+  //         		delete this[i];
+  //         }
+  //     }
+  // }
+  //
+  // Object.prototype.onlyKeys = function(objectToKeep) {
+  // 		for(const i in this) {
+  //     		if(objectToKeep.indexOf(i) === -1) {
+  //         		delete this[i];
+  //         }
+  //     }
+  // }
 
   app.set('port', process.env.PORT || 3000);
   app.set('json spaces', 3);
